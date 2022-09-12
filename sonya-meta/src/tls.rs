39e5cdaff381010b2a207a94de478daa1a4954c7
Mnwa <mihan@panfilov.biz>
2022-09-12 22:55:16 +0300

    Remove openssl

--- sonya-meta/src/tls.rs
+++ sonya-meta/src/tls.rs
@@ -1,14 +1,25 @@
 use crate::config::Tls;
-use openssl::ssl::{SslAcceptor, SslAcceptorBuilder, SslFiletype, SslMethod};
+use rustls::ServerConfig;
+use rustls_pemfile::{certs, pkcs8_private_keys};
+use std::fs::File;
+use std::io::BufReader;
 
-pub fn get_options_from_config(opts: Tls) -> SslAcceptorBuilder {
-    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
-    builder
-        .set_private_key_file(opts.private_key, SslFiletype::PEM)
-        .expect("private key not found");
-    builder
-        .set_certificate_chain_file(opts.cert)
-        .expect("cert not found");
+pub fn get_options_from_config(opts: Tls) -> ServerConfig {
+    let cert_file = &mut BufReader::new(File::open(opts.cert).unwrap());
+    let key_file = &mut BufReader::new(File::open(opts.private_key).unwrap());
 
-    builder
+    let cert = certs(cert_file)
+        .unwrap()
+        .into_iter()
+        .map(rustls::Certificate)
+        .collect();
+
+    let keys = pkcs8_private_keys(key_file).unwrap();
+    let key = rustls::PrivateKey(keys[0].clone());
+
+    ServerConfig::builder()
+        .with_safe_defaults()
+        .with_no_client_auth()
+        .with_single_cert(cert, key)
+        .unwrap()
 }
--- sonya-proxy/src/main.rs
+++ sonya-proxy/src/main.rs
@@ -469,7 +469,7 @@ async fn main() -> std::io::Result<()> {
     let result = futures::future::select(rx, {
         match config.tls {
             None => server.bind(address)?,
-            Some(opts) => server.bind_openssl(address, get_options_from_config(opts))?,
+            Some(opts) => server.bind_rustls(address, get_options_from_config(opts))?,
         }
         .run()
     })
