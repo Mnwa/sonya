15482f0de6fc4612b1d418b25b72b8e1c34bb162
Mnwa <mihan@panfilov.biz>
2022-09-23 16:16:54 +0300

    Rollback to sled

--- sonya/src/queue/map.rs
+++ sonya/src/queue/map.rs
@@ -3,14 +3,16 @@ use dashmap::mapref::one::Ref;
 use dashmap::DashMap;
 use derive_more::{Display, Error, From};
 use futures::stream::BoxStream;
-use log::info;
+use futures::{SinkExt, TryFutureExt, TryStreamExt};
+use log::{error, info};
 use rocksdb::{
-    AsColumnFamilyRef, BlockBasedIndexType, BlockBasedOptions, DBCompressionType, DBWithThreadMode,
-    DataBlockIndexType, Direction, IteratorMode, MemtableFactory, MultiThreaded, Options,
-    ReadOptions, SliceTransform, WriteBatch,
+    AsColumnFamilyRef, BlockBasedIndexType, BlockBasedOptions, DBCompressionType,
+    DataBlockIndexType, Direction, IteratorMode, MemtableFactory, Options, ReadOptions,
+    SliceTransform,
 };
 use serde::de::DeserializeOwned;
 use serde::Serialize;
+use sled::{Batch, IVec, Tree};
 use sonya_meta::config::Queue as QueueOptions;
 use sonya_meta::message::{
     Event, RequestSequence, RequestSequenceId, SequenceEvent, SequenceId, UniqIdEvent,
@@ -22,7 +24,7 @@ use std::sync::atomic::{AtomicU64, Ordering};
 use std::time::{Duration, Instant};
 use tokio::sync::broadcast::{channel, Receiver, Sender};
 
-pub type QueueMap = DBWithThreadMode<MultiThreaded>;
+pub type QueueMap = sled::Db;
 
 #[derive(Debug)]
 pub struct Queue<T> {
@@ -36,34 +38,12 @@ where
     T: 'a + Send + DeserializeOwned + Serialize + Debug + Event + Clone,
 {
     pub fn new(config: QueueOptions) -> QueueResult<Self> {
-        let path = match config.db_path {
-            None => {
-                let mut temp = std::env::temp_dir();
-                temp.push(format!(
-                    "{}_{}",
-                    env!("CARGO_PKG_NAME"),
-                    env!("CARGO_PKG_VERSION")
-                ));
-
-                let r_result = std::fs::remove_dir_all(&temp);
-
-                if matches!(&r_result, Err(e) if e.kind() != ErrorKind::NotFound) {
-                    r_result.unwrap_or_else(|_| {
-                        panic!("fail to clear temp directory: {:?}", temp.to_str())
-                    });
-                }
-
-                temp
-            }
-            Some(dp) => dp,
+        let db_config = match config.db_path {
+            None => sled::Config::new().temporary(true),
+            Some(dp) => sled::Config::new().path(dp).use_compression(true),
         };
 
-        let opts = create_rocks_opts(config.max_key_updates.unwrap_or(1000));
-
-        let mut list = config.default;
-        list.extend(QueueMap::list_cf(&opts, &path).unwrap_or_default());
-
-        let map = QueueMap::open_cf(&opts, path, list)?;
+        let map = db_config.open()?;
 
         let this = Self {
             map,
@@ -71,34 +51,37 @@ where
             queue_meta: Default::default(),
         };
 
+        config
+            .default
+            .into_iter()
+            .try_for_each(|q| this.create_queue(q))?;
+
         Ok(this)
     }
 
     pub fn create_queue(&self, queue_name: String) -> QueueResult<()> {
-        let opts = create_rocks_opts(self.max_key_updates.unwrap_or(1000));
-
         self.map
-            .create_cf(queue_name, &opts)
+            .open_tree(queue_name.as_bytes())
             .map(|_| ())
             .map_err(QueueError::from)
     }
 
     pub fn delete_queue(&self, queue_name: String, id: String) -> QueueResult<()> {
-        let queue = get_queue_broadcast(queue_name.clone(), &self.queue_meta);
+        let mut queue_b = self.queue_broadcasts.lock().unwrap();
+        let queue = get_queue_broadcast(queue_name.clone(), &mut queue_b);
         queue.keys.remove(&id);
 
-        let handle = match self.map.cf_handle(queue_name.as_str()) {
-            None => return Ok(()),
-            Some(h) => h,
-        };
+        let mut batch = Batch::default();
 
-        self.map
-            .delete_range_cf(
-                &handle,
-                get_id(id.as_str(), u64::MIN),
-                get_id(id.as_str(), u64::MAX),
-            )
-            .map_err(QueueError::from)
+        let tree = self.map.open_tree(queue_name.as_bytes())?;
+
+        for response in tree.scan_prefix(id.as_bytes()) {
+            let (key, _) = response?;
+
+            batch.remove(key);
+        }
+
+        tree.apply_batch(batch).map_err(QueueError::from)
     }
 
     pub fn subscribe_queue_by_id(
@@ -107,12 +90,11 @@ where
         id: String,
         sequence: RequestSequence,
     ) -> QueueResult<Subscription<'a, T>> {
-        let handle = match self.map.cf_handle(queue_name.as_str()) {
-            None => return Ok(Default::default()),
-            Some(h) => h,
-        };
+        if !self.check_tree_exists(&queue_name) {
+            return Ok(Default::default());
+        }
 
-        let mut prev_items = get_prev_items::<T>(&self.map, &handle, &id, sequence)?;
+        let mut prev_items = get_prev_items::<T>(&tree, &id, sequence)?;
 
         if let Some(last) = prev_items.as_mut().and_then(|vec| vec.last_mut()) {
             last.set_last(true)
@@ -121,7 +103,7 @@ where
         let prev_len = prev_items.as_ref().map(|i| i.len());
 
         let queue = get_queue_broadcast(queue_name, &self.queue_meta);
-        let key_sender = get_key_broadcast(&self.map, &handle, &id, &queue)?;
+        let key_sender = get_key_broadcast(&id, &queue)?;
 
         let recv = key_sender.sender.subscribe();
 
@@ -136,12 +118,11 @@ where
         queue_name: String,
         sequence: RequestSequence,
     ) -> QueueResult<Subscription<'a, T>> {
-        let handle = match self.map.cf_handle(queue_name.as_str()) {
-            None => return Ok(Default::default()),
-            Some(h) => h,
-        };
+        if !self.check_tree_exists(&queue_name) {
+            return Ok(Default::default());
+        }
 
-        let mut prev_items = get_prev_all_items::<T>(&self.map, &handle, sequence)?;
+        let mut prev_items = get_prev_all_items::<T>(&self.map, sequence)?;
 
         if let Some(last) = prev_items.as_mut().and_then(|vec| vec.last_mut()) {
             last.set_last(true)
@@ -159,25 +140,16 @@ where
         })
     }
 
-    pub fn send_to_queue(
-        &self,
-        queue_name: String,
-        mut value: T,
-    ) -> QueueResult<(bool, Option<SequenceId>)> {
-        let time = Instant::now();
-        let handle = match self.map.cf_handle(queue_name.as_str()) {
-            None => return Ok((false, None)),
-            Some(h) => h,
-        };
+    pub fn send_to_queue(&self, queue_name: String, mut value: T) -> QueueResult<bool> {
+        if !self.check_tree_exists(&queue_name) {
+            return Ok(false);
+        }
 
-        let handle_time = time.elapsed();
-
-        let queue = get_queue_broadcast(queue_name, &self.queue_meta);
-        let key_sender = get_key_broadcast(&self.map, &handle, value.get_id(), &queue)?;
+        let id = value.get_id();
 
         let sequence = match value.get_sequence() {
             None => {
-                let id = self.generate_next_id(&key_sender);
+                let id = self.generate_next_id(&queue_name, id)?;
 
                 value.set_sequence(id);
 
@@ -186,82 +158,79 @@ where
             Some(s) => s.get(),
         };
 
-        let sequence_time = time.elapsed();
-
-        let mut write_time = Duration::default();
-        let mut clear_time = Duration::default();
-
         if !matches!(self.max_key_updates, Some(0)) {
             let id = get_id(value.get_id(), sequence);
 
-            let mut batch = WriteBatch::default();
+            let tree = self.map.open_tree(queue_name.as_bytes())?;
 
-            batch.put_cf(&handle, id, rmp_serde::to_vec(&value)?);
+            tree.insert(id, serde_json::to_vec(&value)?)?;
 
             if let Some(m) = self.max_key_updates {
-                let min_id = get_id(value.get_id(), u64::MIN);
-                let max_id = get_id(value.get_id(), sequence);
+                let mut batch = Batch::default();
 
-                let mut opts = ReadOptions::default();
-                opts.set_iterate_lower_bound(min_id);
-                opts.set_prefix_same_as_start(true);
-
-                self.map
-                    .snapshot()
-                    .iterator_cf_opt(
-                        &handle,
-                        opts,
-                        IteratorMode::From(&max_id, Direction::Reverse),
-                    )
+                tree.scan_prefix(value.get_id().as_bytes())
+                    .rev()
                     .skip(m - 1)
                     .try_for_each::<_, QueueResult<()>>(|r| {
                         let (k, _) = r?;
-                        batch.delete_cf(&handle, k);
+                        batch.remove(k);
                         Ok(())
                     })?;
 
-                clear_time = time.elapsed();
+                tree.apply_batch(batch)?;
             }
-
-            self.map.write(batch)?;
-
-            write_time = time.elapsed();
         }
 
-        let _ = queue.sender.send(value.clone());
-        let _ = key_sender.sender.send(value);
+        let mut map = self.queue_broadcasts.lock().unwrap();
 
-        let broadcast_time = time.elapsed();
+        let queue = get_queue_broadcast(queue_name, &mut map);
+        if let Err(e) = queue.sender.send(value.clone()) {
+            error!("broadcast message to queue subscribers error: {}", e)
+        }
 
-        info!(
-            "handle: {}, sequence: {}, clear: {}, write: {}, broadcast: {}",
-            handle_time.as_millis(),
-            sequence_time.as_millis() - handle_time.as_millis(),
-            clear_time
-                .as_millis()
-                .checked_div(sequence_time.as_millis())
-                .unwrap_or(0),
-            write_time
-                .as_millis()
-                .checked_div(clear_time.max(sequence_time).as_millis())
-                .unwrap_or(0),
-            broadcast_time.as_millis() - write_time.max(sequence_time).as_millis(),
-        );
+        let key_sender = get_key_broadcast(value.get_id(), &queue);
+        if let Err(e) = key_sender.send(value) {
+            error!("broadcast message to key subscribers error: {}", e)
+        }
 
-        Ok((true, SequenceId::new(sequence)))
+        Ok(true)
     }
 
     pub fn close_queue(&self, queue_name: String) -> QueueResult<bool> {
-        self.queue_meta.remove(&queue_name);
+        let mut queue_b = self.queue_broadcasts.lock().unwrap();
+        queue_b.remove(&queue_name);
+        queue_b.remove(&queue_name);
 
-        self.map
-            .drop_cf(queue_name.as_str())
-            .map_err(QueueError::from)
-            .map(|_| true)
+        self.map.drop_tree(queue_name).map_err(QueueError::from)
     }
 
-    fn generate_next_id(&self, key_sender: &KeyBroadcast<T>) -> SequenceId {
-        SequenceId::new(key_sender.sequence.fetch_add(1, Ordering::SeqCst)).unwrap()
+    fn check_tree_exists(&self, queue_name: &str) -> bool {
+        matches!(
+            self.map
+                .tree_names()
+                .into_iter()
+                .find(|v| v == queue_name.as_bytes()),
+            Some(_)
+        )
+    }
+
+    fn generate_next_id(&self, queue_name: &str, id: &str) -> QueueResult<SequenceId> {
+        let mut key = Vec::from("id_");
+        key.extend_from_slice(queue_name.as_bytes());
+        key.extend_from_slice(id.as_bytes());
+
+        let res = self.map.update_and_fetch(key, |v| {
+            v.and_then(|v| Some(u64::from_be_bytes(v.try_into().ok()?)))
+                .and_then(|id| id.checked_add(1))
+                .map(|id| IVec::from(&id.to_be_bytes()))
+                .unwrap_or_else(|| IVec::from(&1u64.to_be_bytes()))
+                .into()
+        })?;
+
+        res.and_then(|r| Some(u64::from_be_bytes(r.as_ref().try_into().ok()?)))
+            .and_then(SequenceId::new)
+            .map(Ok)
+            .unwrap_or_else(|| Err(QueueError::ZeroSequence))
     }
 }
 
@@ -291,91 +260,45 @@ fn get_id(id: &str, sequence: u64) -> Vec<u8> {
 }
 
 fn get_prev_items<T: DeserializeOwned>(
-    map: &QueueMap,
-    cf_handle: &impl AsColumnFamilyRef,
+    tree: &Tree,
     id: &str,
-    sequence: RequestSequence,
-) -> QueueResult<Option<Vec<T>>> {
-    sequence
-        .map(|sequence_id| extract_sequences::<T>(map, cf_handle, sequence_id, id))
-        .transpose()
-}
-
-fn extract_sequences<T: DeserializeOwned>(
-    map: &QueueMap,
-    cf_handle: &impl AsColumnFamilyRef,
-    sequence_id: RequestSequenceId,
-    id: &str,
-) -> Result<Vec<T>, QueueError> {
-    let mut opts = ReadOptions::default();
-
-    let min_id = get_id(id, u64::MIN);
-    let max_id = get_id(id, u64::MAX);
-
-    opts.set_ignore_range_deletions(true);
-    opts.set_prefix_same_as_start(true);
-
-    let snapshot = map.snapshot();
-
-    let iter: Box<dyn Iterator<Item = Result<(Box<[u8]>, Box<[u8]>), rocksdb::Error>>> =
-        match sequence_id {
-            RequestSequenceId::Id(s) => {
-                opts.set_iterate_upper_bound(max_id.clone());
-
-                Box::new(snapshot.iterator_cf_opt(
-                    cf_handle,
-                    opts,
-                    IteratorMode::From(&get_id(id, s.get()), Direction::Forward),
-                ))
-            }
-            RequestSequenceId::Last => {
-                opts.set_iterate_lower_bound(min_id.clone());
-
-                Box::new(
-                    snapshot
-                        .iterator_cf_opt(
-                            cf_handle,
-                            opts,
-                            IteratorMode::From(&max_id, Direction::Reverse),
-                        )
-                        .take(1),
-                )
-            }
-            RequestSequenceId::First => {
-                opts.set_iterate_upper_bound(max_id.clone());
-
-                Box::new(snapshot.iterator_cf_opt(
-                    cf_handle,
-                    opts,
-                    IteratorMode::From(&min_id, Direction::Forward),
-                ))
-            }
-        };
-
-    iter.map(|r| {
-        r.map(|(_, v)| v)
-            .map_err(QueueError::from)
-            .and_then(|v| rmp_serde::from_slice(&v).map_err(QueueError::from))
-    })
-    .collect()
-}
-
-fn get_prev_all_items<T: DeserializeOwned + SequenceEvent + UniqIdEvent>(
-    map: &QueueMap,
-    cf_handle: &impl AsColumnFamilyRef,
     sequence: RequestSequence,
 ) -> QueueResult<Option<Vec<T>>> {
     sequence
         .map(|sequence_id| {
-            let mut opts = ReadOptions::default();
-            opts.set_ignore_range_deletions(true);
+            extract_sequences(tree, sequence_id, id)
+                .map(|r| {
+                    r.map(|(_, v)| v)
+                        .map_err(QueueError::from)
+                        .and_then(|v| serde_json::from_slice(&v).map_err(QueueError::from))
+                })
+                .collect()
+        })
+        .transpose()
+}
 
-            let i = map
-                .iterator_cf_opt(cf_handle, opts, IteratorMode::Start)
-                .map(|v| {
-                    v.map_err(QueueError::from)
-                        .and_then(|(_, v)| rmp_serde::from_slice(&v).map_err(QueueError::from))
-                });
+fn extract_sequences(
+    tree: &Tree,
+    sequence_id: RequestSequenceId,
+    id: &str,
+) -> Box<dyn Iterator<Item = sled::Result<(IVec, IVec)>>> {
+    match sequence_id {
+        RequestSequenceId::Id(s) => Box::new(tree.range(get_id(id, s.get())..get_id(id, u64::MAX))),
+        RequestSequenceId::Last => Box::new(tree.scan_prefix(id.as_bytes()).rev().take(1)),
+        RequestSequenceId::First => Box::new(tree.scan_prefix(id.as_bytes())),
+    }
+}
+
+fn get_prev_all_items<T: DeserializeOwned + UniqId>(
+    tree: &Tree,
+    sequence: RequestSequence,
+) -> QueueResult<Option<Vec<T>>> {
+    sequence
+        .map(|sequence_id| {
+            let i = tree.iter().values().map(|v| {
+                v.map_err(QueueError::from)
+                    .and_then(|v| serde_json::from_slice(&v).map_err(QueueError::from))
+            });
 
             let i: Box<dyn Iterator<Item = Result<T, QueueError>>> = match sequence_id {
                 RequestSequenceId::Id(s) => {
@@ -408,7 +331,7 @@ fn get_prev_all_items<T: DeserializeOwned + SequenceEvent + UniqIdEvent>(
 
 #[derive(Debug, Display, From, Error)]
 pub enum QueueError {
-    Db(rocksdb::Error),
+    Db(sled::Error),
     Encode(rmp_serde::encode::Error),
     Decode(rmp_serde::decode::Error),
     #[display(fmt = "sequence must be more then 0")]
@@ -427,7 +350,6 @@ struct QueueBroadcast<T> {
 #[derive(Debug)]
 struct KeyBroadcast<T> {
     sender: Sender<T>,
-    sequence: AtomicU64,
 }
 
 // Potentially may be replaced with consistent entry and downgrade
@@ -452,47 +374,22 @@ fn get_queue_broadcast<T: Clone>(
 
 // Potentially may be replaced with consistent entry and downgrade
 fn get_key_broadcast<'a, T: Clone + SequenceEvent + DeserializeOwned>(
-    map: &'a QueueMap,
-    cf_handle: &'a impl AsColumnFamilyRef,
     id: &str,
     queue_broadcast: &'a QueueBroadcast<T>,
-) -> QueueResult<Ref<'a, String, KeyBroadcast<T>>> {
+) -> Ref<'a, String, KeyBroadcast<T>> {
     if !queue_broadcast.keys.contains_key(id) {
-        let mut opts = ReadOptions::default();
-        opts.set_ignore_range_deletions(true);
-        opts.set_iterate_lower_bound(get_id(id, u64::MIN));
-        opts.set_iterate_upper_bound(get_id(id, u64::MAX));
-
-        let last: Option<T> = map
-            .iterator_cf_opt(cf_handle, opts, IteratorMode::End)
-            .next()
-            .transpose()
-            .map_err(QueueError::from)
-            .and_then(|b| match b {
-                None => Ok(None),
-                Some((_, v)) => rmp_serde::from_slice::<T>(&v)
-                    .map_err(QueueError::from)
-                    .map(Some),
-            })?;
-
-        let sequence = last
-            .and_then(|v| v.get_sequence())
-            .map(|s| AtomicU64::new(s.get()))
-            .unwrap_or_else(|| AtomicU64::new(1));
-
         queue_broadcast.keys.insert(
             id.to_string(),
             KeyBroadcast {
                 sender: channel(1024).0,
-                sequence,
             },
         );
     }
 
-    Ok(queue_broadcast
+    queue_broadcast
         .keys
         .get(id)
-        .expect("data race occurred, keys broadcast already dropped"))
+        .expect("data race occurred, keys broadcast already dropped")
 }
 
 pub struct Subscription<'a, T> {
